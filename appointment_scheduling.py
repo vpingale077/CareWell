b7f3e90d1033f8a3edb63ced09838308c019206d
Vaibhav Pingale <vpingale07@gmail.com>
2024-08-01 20:41:24 +0000

    modified:   appointment_scheduling.py 	new file:   operations/hospital_operations.py

--- appointment_scheduling.py
+++ appointment_scheduling.py
@@ -1,31 +1,65 @@
 import json
-from util import get_date_in_format, is_valid_date
+from turtle import st
+from operations.hospital_operations import book_doctors_appointment
+from util import get_date_in_format, is_valid_date, setChatMsg
 
 
-def book_appointment(raw_parameters,AI71_API_KEY):
-    required_keys = ["doctor", "date"]
-    parameters= {k.lower(): v for k, v in raw_parameters.items()}
+def book_appointment(raw_parameters: dict, ai71_api_key: str) -> tuple:
+    """
+    Books an appointment with a doctor.
+
+    Args:
+    - raw_parameters (dict): A dictionary containing the appointment details.
+    - ai71_api_key (str): The AI71 API key.
+
+    Returns:
+    - tuple: A tuple containing a boolean indicating whether the appointment was booked successfully and a message.
+    """
+
+    # Define the required keys for the appointment
+    required_keys = ["doctor", "date", "time", "location"]
+
+    # Convert the raw parameters to lowercase
+    parameters = {k.lower(): v for k, v in raw_parameters.items()}
+
+    # Check each required key and ask the user to share details if missing
+    missing_keys = [key for key in required_keys if key not in parameters]
+    if missing_keys:
+        resp = f"Please provide the following details for appointment scheduling: {', '.join(missing_keys)}"
+        setChatMsg(resp)
+
+    # Check if the doctor and location are strings
+    if not all(isinstance(parameters[key], str) for key in ["doctor", "location"]):
+       resp = "Invalid data types for doctor or location"
+       setChatMsg(resp)
+
+    # Extract the doctor name, date string, time string, and location string
     doctor_name = parameters.get("doctor")
     date_string = parameters.get("date")
     time_string = parameters.get("time")
     location_string = parameters.get("location")
 
-    if not all(key in parameters for key in required_keys):
-        return False, "Please provide details for appointment scheduling"
-
-    if not all(isinstance(parameters[key], str) for key in ["doctor", "location"]):
-        return False, "Invalid data types for doctor or location"
-
-    #date = parameters["date"]
+    # Check if the date is valid
     if is_valid_date(date_string):
-        return True, f"Appointment scheduled for {doctor_name} on {date_string} {time_string} in {location_string}"
+        # Book the appointment
+        is_app_booked, response = book_doctors_appointment(doctor_name, date_string, time_string, location_string)
+        if is_app_booked:
+            resp=f"Appointment scheduled for {doctor_name} on {date_string} {time_string} in {location_string}"
+            setChatMsg(resp)
+        else:
+            
+            return False, response
 
-    date_updated = json.loads(get_date_in_format(date_string,AI71_API_KEY).choices[0].message.content)
-    print(date_updated["date"])
-    if (len(date_updated["date"])) > 1:
+    # Get the updated date
+    date_updated = json.loads(get_date_in_format(date_string, ai71_api_key).choices[0].message.content)
+
+    # Check if the date is valid
+    if len(date_updated["date"]) > 1:
         return True, f"Please specify date for Appointment with {doctor_name} in {location_string}"
 
+    # Check if the date is not invalid
     if date_updated["date"] != "invalid":
         return True, f"Appointment scheduled for {doctor_name} on {date_updated['date'][0]} in {location_string}"
 
+    # Return an error message if the date is invalid
     return False, "Please provide date of appointment"
---
+++ operations/hospital_operations.py
@@ -0,0 +1,38 @@
+import datetime
+import streamlit as st
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+md_username = st.secret["MD_USER"]
+md_password = st.secret["MD_PASS"]
+md_db = st.secret["MD_DB"]
+md_doc_col= st.secret["MD_DOC_COL"]
+md_ap_col = st.secret["MD_AP_COL"]
+
+uri = "mongodb+srv://{md_username}:{md_password}@cluster0.tgg39md.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+def book_doctors_appointment(doctor_name,date_string,time_string,location_string):
+    print("book_doctors_appointment")
+    client = MongoClient(uri, server_api=ServerApi('1'))
+    try:
+        client.admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+        database = client[md_db]
+        doctor_collection = database[md_doc_col]
+        appointment_collection = database[md_ap_col]
+
+        doctors = doctor_collection.find({},{"doctor": doctor_name})
+        current_date = datetime.now().date()
+        if not doctors:
+            print(f"Doctors data not present.")
+            return False, f"Kindly recheck doctor's name. Looks like we don't have doctor {doctor_name} in our system."
+        else:
+            for doc in doctors:
+                print(doc["_id"])
+                appointment = appointment_collection.find({},{"doctor_id":doc,"date":date_string})
+                for slots in appointment:
+                    print(slots)
+            return True, f"Appointment booked"    
+            
+    except Exception as e:
+        print(e)
